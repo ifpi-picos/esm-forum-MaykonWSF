b205f5b2dea445fd664aada9caa1e7b44a57aa0f
Maykon Willyam <maykonwillyam038@gmail.com>
2025-04-05 02:57:06 -0300

    configurando e iniciando confecção do código para realizar testes

---
+++ fonte/logica/modelo.js
@@ -0,0 +1,43 @@
+let repositorio = require('../../repositorio/memoria');
+
+function reconfig_repositorio(novoRepositorio) {
+  repositorio = novoRepositorio;
+}
+
+async function listar_perguntas() {
+  const perguntas = await repositorio.recuperar_todas_perguntas();
+  for (const pergunta of perguntas) {
+    pergunta.num_respostas = await repositorio.recuperar_num_respostas(pergunta.id_pergunta);
+  }
+  return perguntas;
+}
+
+async function cadastrar_pergunta(texto) {
+  return await repositorio.criar_pergunta(texto);
+}
+
+async function cadastrar_resposta(id_pergunta, texto) {
+  return await repositorio.criar_resposta(id_pergunta, texto);
+}
+
+async function get_pergunta(id_pergunta) {
+  return await repositorio.recuperar_pergunta(id_pergunta);
+}
+
+async function get_respostas(id_pergunta) {
+  return await repositorio.recuperar_todas_respostas(id_pergunta);
+}
+
+async function get_num_respostas(id_pergunta) {
+  return await repositorio.recuperar_num_respostas(id_pergunta);
+}
+
+module.exports = {
+  reconfig_repositorio,
+  listar_perguntas,
+  cadastrar_pergunta,
+  cadastrar_resposta,
+  get_pergunta,
+  get_respostas,
+  get_num_respostas
+};
